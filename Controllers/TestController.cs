74ddddfdb4c79e2171d0529919a06c09765b4e78
Cocos Darius <cocodarius04@yahoo.com>
2026-03-25 20:14:18 +0200

    setup api base + dbcontext + swagger working

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventHub.Data;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public TestController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("DbContext works");
+    }
+}
